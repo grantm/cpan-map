3da5a77f4dd08778ec0e79fbf96ebca39970c0f6
Grant McLean <grant@mclean.net.nz>
2011-11-09 21:50:56 +1300

    implement zoom +/-

--- html/static/cpan-map.js
+++ html/static/cpan-map.js
@@ -29,28 +29,33 @@
     };
 
     $.fn.cpan_map.defaults = {
-        app_title:    'Map of CPAN',
-        map_data_url: 'cpan-map-data.json'
+        app_title        : 'Map of CPAN',
+        zoom_minus_label : 'Zoom map out',
+        zoom_plus_label  : 'Zoom map in',
+        map_data_url     : 'cpan-map-data.json',
+        map_margin       : 50,
+        zoom_scales      : [ 2, 3, 4, 5, 6, 8, 10 ]  // Must match CSS zoom*
     };
 
 
     // Application globals
 
-    var zoom_scale = [ 2, 3, 4, 5, 6, 8, 10 ];  // Must match CSS zoom*
-    var scale      = 30;
-    var map_margin = 50;
-    var distros, mass_map;
-    var dist_data, plane_rows, plane_cols;
+    var distros, mass_map, plane_rows, plane_cols;
 
+
+    function app_options($app) {
+        return $app.data('options');
+    }
 
     function build_app(app) {
         var $app      = $(app);
-        var $zoom     = $('<ul class="map-zoom" />');
+        var opt       = app_options($app);
+        var $controls = $('<div class="map-controls" />');
         var $viewport = $('<div class="map-viewport loading" />');
         $app.addClass('cpan-map');
         $app.append(
-            $('<h1 />').text( $app.data('options').app_title ),
-            $zoom,
+            $('<h1 />').text( opt.app_title ),
+            $controls,
             $viewport.html('<div class="init">Loading map data ...</div>')
         );
         $.ajax({
@@ -61,6 +66,7 @@
     }
 
     function populate_map($app, data) {
+        var opt = app_options($app);
         var $viewport = $app.find('.map-viewport');
         mass_map   = data.mass_map;
         distros    = data.distros.data;
@@ -68,8 +74,6 @@
         plane_cols = data.distros.cols;
         var $plane = $('<table class="map-plane" />');
 
-plane_rows = 8;
-plane_cols = 8;
         for(var i = 0; i < plane_rows; i++) {
             var $row = $('<tr />');
             for(var j = 0; j < plane_cols; j++) {
@@ -89,23 +93,36 @@ plane_cols = 8;
         $viewport.html('');
         $viewport.append($plane);
         $plane.draggable({ });
+        add_controls($app);
         set_zoom($app, 6);
     }
 
-    function set_zoom($app, zoom) {
+    function set_zoom($app, new_zoom) {
+        var opt = app_options($app);
+        zoom_scales = opt.zoom_scales;
+        if(new_zoom < 0) {
+            new_zoom = 0;
+        }
+        if(new_zoom >= zoom_scales.length) {
+            new_zoom = zoom_scales.length - 1;
+        }
+        if(new_zoom === opt.current_zoom) {
+            return;
+        }
+        opt.current_zoom = new_zoom;
+        opt.scale = zoom_scales[new_zoom];
         var $plane = $app.find('.map-plane');
-        var i = parseInt(zoom);
-        scale = zoom_scale[i - 1];
-        $plane.width(scale * plane_cols);
-        for(var z = 1; z <= 5; z++) {
+        var i = parseInt(new_zoom);
+        $plane.width(opt.scale * plane_cols);
+        for(var z = 1; z < opt.zoom_scales.length; z++) {
             $plane.removeClass('zoom' + z);
         }
-        $plane.addClass('zoom' + zoom);
+        $plane.addClass('zoom' + new_zoom);
         size_viewport($app);
     }
 
     function dist_colour(dist) {
-        var ns = dist.ns;
+        var ns   = dist.ns;
         var mass = mass_map[ns];
         if(mass) {
             return 'c' + mass.colour;
@@ -116,8 +133,9 @@ plane_cols = 8;
     }
 
     function size_viewport($app) {
+        var opt = app_options($app);
         var $viewport = $app.find('.map-viewport');
-        var plane  = plane_dimensions();
+        var plane  = plane_dimensions(opt);
         var wrap   = $viewport.offset();
         var border = parseInt($viewport.css('border-left-width'));
         var width  = $(window).width() - (wrap.left * 2) - (border * 2);
@@ -127,7 +145,7 @@ plane_cols = 8;
         if(width < 100) {
             width = 100;
         }
-        height = $(window).height() - wrap.top - 20;;
+        var height = $(window).height() - wrap.top - 20;;
         if(height > plane.height) {
             height = plane.height;
         }
@@ -140,34 +158,47 @@ plane_cols = 8;
     }
 
     function reset_drag_range($app) {
+        var opt = app_options($app);
         var $viewport = $app.find('.map-viewport');
         var $plane    = $app.find('.map-plane');
         var wrap      = $viewport.offset();
         wrap.width    = $viewport.innerWidth();
         wrap.height   = $viewport.innerHeight();
-        var plane     = plane_dimensions();
+        var plane     = plane_dimensions(opt);
         $plane.draggable( 'option', 'containment', [
-            wrap.left - (plane.width  + map_margin - wrap.width),
-            wrap.top  - (plane.height + map_margin - wrap.height),
-            wrap.left + map_margin,
-            wrap.top  + map_margin
+            wrap.left - (plane.width  + opt.map_margin - wrap.width),
+            wrap.top  - (plane.height + opt.map_margin - wrap.height),
+            wrap.left + opt.map_margin,
+            wrap.top  + opt.map_margin
         ]);
     }
 
-    function plane_dimensions() {
+    function add_controls($app) {
+        var opt = $app.data('options');
+        var $zoom = $('<ul class="map-zoom" />');
+        $app.find('.map-controls').append(
+            $zoom.append(
+                $('<li class="zoom-minus">-</li>')
+                    .attr('title', opt.zoom_minus_label)
+                    .click(function() { inc_zoom($app, -1) }),
+                $('<li class="zoom-plus">+</li>')
+                    .attr('title', opt.zoom_plus_label)
+                    .click(function() { inc_zoom($app, 1) })
+            )
+        );
+    }
+
+    function plane_dimensions(opt) {
         return {
-            width:  plane_cols * scale,
-            height: plane_rows * scale
+            width:  plane_cols * opt.scale,
+            height: plane_rows * opt.scale
         };
+    }
+
+    function inc_zoom($app, inc) {
+        var opt = app_options($app);
+        set_zoom($app, opt.current_zoom + inc);
     }
 
 })(jQuery);
 
-
-var old = function($) {
-
-    $('ul#map-zoom li').click(function() {
-        set_zoom($(this).text());
-    });
-
-}
