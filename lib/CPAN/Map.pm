7d276965bd28bdd6b2ffa6b09b3353c0bc210580
Grant McLean <grant@mclean.net.nz>
2011-12-03 11:50:49 +1300

    add code and docs to CPAN/Map.pm

--- lib/CPAN/Map.pm
+++ lib/CPAN/Map.pm
@@ -3,52 +3,56 @@ package CPAN::Map;
 use warnings;
 use strict;
 
+use CPAN::Map::Builder;
+
+
 =head1 NAME
 
-CPAN::Map - The great new CPAN::Map!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
-
+CPAN::Map - The code behind mapofcpan.org
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
     use CPAN::Map;
 
-    my $foo = CPAN::Map->new();
-    ...
+    CPAN::Map->build(
+        # options, e.g.:
+        verbose => 1,
+    );
 
-=head1 EXPORT
+=head1 DESCRIPTION
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+The Map of CPAN is a web application for visualising and exploring
+distributions on CPAN.  Although the map provides a dynamic, interactive user
+interface, the application's AJAX requests are serviced directly by
+api.metacpan.org, so this source package does not include any server-side code.
+The role of this package is to generate the static data files and images used
+by the map.
 
-=head1 SUBROUTINES/METHODS
+=head1 METHODS
 
-=head2 function1
+=head2 build
+
+The C<build> method coordinates parsing the base data out of the
+F<modules/02packages.details.txt.gz> and F<authors/01mailrc.txt.gz> files from
+CPAN and using this data to generate map images and a summary data file for
+consumption by the Javascript code.
+
+This method is a thin wrapper around L<CPAN::Map::Builder>.  All supplied
+arguments are passed to the CPAN::Map::Builder constructor.
 
 =cut
 
-sub function1 {
+sub build {
+    my $class   = shift;
+    my $builder = CPAN::Map::Builder->generate( @_ );
 }
 
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-CPAN::Map at rt.cpan.org>,
-or through the web interface at
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=CPAN::Map>.  I will be
-notified, and then you'll automatically be notified of progress on your bug as
-I make changes.
+Please report any bugs or feature requests through the github issue tracker at
+L<https://github.com/grantm/cpan-map/issues>.  I will be notified, and then
+you'll automatically be notified of progress on your bug as I make changes.
 
 
 =head1 SUPPORT
@@ -61,9 +65,13 @@ You can also look for information at:
 
 =over 4
 
-=item * RT: CPAN's request tracker
+=item * source repository
 
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=CPAN::Map>
+L<https://github.com/grantm/cpan-map/>
+
+=item * issue tracker
+
+L<https://github.com/grantm/cpan-map/issues>
 
 =item * AnnoCPAN: Annotated CPAN documentation
 
@@ -73,26 +81,27 @@ L<http://annocpan.org/dist/CPAN::Map>
 
 L<http://cpanratings.perl.org/d/CPAN::Map>
 
-=item * Search CPAN
+=item * MetaCPAN
 
-L<http://search.cpan.org/dist/CPAN::Map/>
+L<https://metacpan.org/release/CPAN-Map>
 
 =back
 
 
 =head1 ACKNOWLEDGEMENTS
 
+The Map of CPAN application relies heavily on the web API provided by
+metacpan.org.
 
 =head1 COPYRIGHT AND LICENSE
 
 Copyright 2011 Grant McLean C<< <grantm@cpan.org> >>
 
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
-
-See http://dev.perl.org/licenses/ for more information.
-
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License, Version 3 or later by the
+Free Software Foundation.  You should have received a copy of the GNU Affero
+General Public License along with this program.  If not, see
+L<http://www.gnu.org/licenses/>.
 
 =cut
 
