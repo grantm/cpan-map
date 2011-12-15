5705960c6cf71de730a33e6b38a78c93b2709a16
Grant McLean <grant@mclean.net.nz>
2011-12-15 23:07:22 +1300

    enable basic authentication

--- puppet/modules/cpan-map/manifests/nginx.pp
+++ puppet/modules/cpan-map/manifests/nginx.pp
@@ -18,17 +18,25 @@ class cpan-map::nginx {
             source  => "puppet:///modules/cpan-map/etc/nginx/nginx.conf",
             owner   => root,
             group   => root,
+            mode    => 640,
             notify  => Exec["nginx-reload"];
 
         '/etc/nginx/sites-available/cpan-map':
             source  => "puppet:///modules/cpan-map/etc/nginx/cpan-map.conf",
             owner   => root,
             group   => root,
+            mode    => 640,
             notify  => Exec["nginx-reload"];
 
         '/etc/nginx/sites-enabled/cpan-map':
             ensure  => '../sites-available/cpan-map',
             notify  => Exec["nginx-reload"];
+
+        '/etc/nginx/htpasswd':
+            owner   => root,
+            group   => www-data,
+            mode    => 640,
+            content => "treasure:qJRb6lIMLQRug\n";
 
     }
 
