531e8cd2f3e6c323f3f2836913de5bff32fcf46b
Grant McLean <grant@mclean.net.nz>
2011-12-17 17:48:10 +1300

    set up PERL5LIB for cron job

--- puppet/modules/cpan-map/manifests/cron.pp
+++ puppet/modules/cpan-map/manifests/cron.pp
@@ -11,6 +11,9 @@ class cpan-map::cron {
             group   => root,
             mode    => 644,
             content => "# Managed by puppet
+
+PERL5LIB=/home/grant/projects/cpan-map/lib
+
 6 4,12,20    * * *   grant    /home/grant/projects/cpan-map/bin/cpan-map --refresh-data 2>&1 | mail -e -s \"CPAN Map data update\" grant@mclean.net.nz
 \n";
 
