9ca4ffaac550418a1c1aa6b62ca121753695b183
Grant McLean <grant@mclean.net.nz>
2012-10-12 22:19:06 +1300

    refactor to allow overriding to generate history frames

--- lib/CPAN/Map/WriteMapImages.pm
+++ lib/CPAN/Map/WriteMapImages.pm
@@ -32,17 +32,43 @@ has 'scale' => (
     isa      => 'Int',
 );
 
+has 'image_width' => (
+    is       => 'rw',
+    isa      => 'Int',
+);
+
+has 'image_height' => (
+    is       => 'rw',
+    isa      => 'Int',
+);
+
+has 'x_offset' => (
+    is       => 'rw',
+    isa      => 'Int',
+    lazy     => 1,
+    default  => 0,
+);
+
+has 'y_offset' => (
+    is       => 'rw',
+    isa      => 'Int',
+    lazy     => 1,
+    default  => 0,
+);
+
+has 'colour' => (
+    is       => 'rw',
+    isa      => 'Ref',
+    lazy     => 1,
+    default  => sub { {} },
+);
+
 
 use constant LINE_THICKNESS => 1;
 use constant NORTH          => 1;
 use constant SOUTH          => 2;
 use constant EAST           => 4;
 use constant WEST           => 8;
-
-my(
-    $bg_colour, $label_colour, $thin_label_colour,
-    $shadow_colour, $border_colour, @map_colour
-);
 
 
 sub write {
@@ -72,39 +98,54 @@ sub purge_old_files {
 sub write_image_file {
     my($self, $builder, $scale) = @_;
 
-    my $output_path = File::Spec->catfile(
-        $self->output_dir, $self->output_filename
-    );
-    my $slug = $builder->slug_of_the_day;
-    $output_path =~ s{(?=[.]png$)}{-$scale-$slug};
+    my $output_path = $self->image_file_path($builder, $scale);
     $builder->progress_message(" - writing PNG image to $output_path");
+
+
+    # Set up image dimensions
+
+    my $rows = $builder->plane_rows;
+    my $cols = $builder->plane_cols;
+
+    $self->image_width($cols * $scale);
+    $self->image_height($rows * $scale);
+
+    my $im = $self->render_image($builder, $scale);
+
+    # Write image out to file
+
+    open my $out, '>', $output_path or die "open($output_path): $!";
+    binmode($out);
+    print $out $im->png;
+    close($out);
+}
+
+
+sub render_image {
+    my($self, $builder, $scale) = @_;
 
     my $font = $builder->label_font_path or $builder->warning_message(
         "can't add map labels without label_font_path"
     );
 
 
-    # Set up image dimensions, colour map and fill the background
+    # Set up colour map and fill the background
 
-    my $rows = $builder->plane_rows;
-    my $cols = $builder->plane_cols;
+    my $im = new GD::Image($self->image_width, $self->image_height);
 
-    my $im = new GD::Image($cols * $scale, $rows * $scale);
+    my $colour = $self->colour;
+    $colour->{background} = $im->colorAllocate(0x66, 0x66, 0x66);
+    $colour->{label}      = $im->colorAllocate(0x44, 0x44, 0x44);
+    $colour->{thin_label} = $im->colorAllocate(0x66, 0x66, 0x66);
+    $colour->{shadow}     = $im->colorAllocate(0xEE, 0xEE, 0xEE);
+    $colour->{border}     = $im->colorAllocate(0x55, 0x55, 0x55);
+    $colour->{map_0}      = $im->colorAllocate(0xBB, 0xDD, 0xFF);
+    $colour->{map_1}      = $im->colorAllocate(0x7A, 0xFF, 0x67);
+    $colour->{map_2}      = $im->colorAllocate(0xFF, 0xE9, 0x3D);
+    $colour->{map_3}      = $im->colorAllocate(0xFF, 0x97, 0xA6);
+    $colour->{map_4}      = $im->colorAllocate(0xFF, 0x87, 0x49);
 
-    $bg_colour         = $im->colorAllocate(0x66, 0x66, 0x66);
-    $label_colour      = $im->colorAllocate(0x44, 0x44, 0x44);
-    $thin_label_colour = $im->colorAllocate(0x66, 0x66, 0x66);
-    $shadow_colour     = $im->colorAllocate(0xEE, 0xEE, 0xEE);
-    $border_colour     = $im->colorAllocate(0x55, 0x55, 0x55);
-    @map_colour = (
-        $im->colorAllocate(0xBB, 0xDD, 0xFF),
-        $im->colorAllocate(0x7A, 0xFF, 0x67),
-        $im->colorAllocate(0xFF, 0xE9, 0x3D),
-        $im->colorAllocate(0xFF, 0x97, 0xA6),
-        $im->colorAllocate(0xFF, 0x87, 0x49),
-    );
-
-    $im->fill(0, 0, $bg_colour);
+    $im->fill(0, 0, $colour->{background});
 
     $im->setThickness(LINE_THICKNESS);
 
@@ -117,10 +158,16 @@ sub write_image_file {
         }
         return 0;
     };
+    my $dist_ns = sub {
+        if(my $ns = $builder->namespace_for_distro($_[0])) {
+            return $ns->name;
+        }
+        return '';
+    };
     $builder->each_distro(sub {
         my($distro) = @_;
-        my $colour = $map_colour[ $dist_colour->($distro) ];
-        my $borders = $self->border_flags($distro, $builder, $dist_colour);
+        my $colour = $colour->{'map_' . $dist_colour->($distro)};
+        my $borders = $self->border_flags($distro, $builder, $dist_ns);
         $self->draw_distro($im, $distro->col, $distro->row, $colour, $borders);
     });
 
@@ -135,20 +182,27 @@ sub write_image_file {
         });
     }
 
+    return $im;
+}
 
-    # Write image out to file
 
-    open my $out, '>', $output_path or die "open($output_path): $!";
-    binmode($out);
-    print $out $im->png;
-    close($out);
+sub image_file_path {
+    my($self, $builder, $scale) = @_;
+
+    my $image_path = File::Spec->catfile(
+        $self->output_dir, $self->output_filename
+    );
+    my $slug = $builder->slug_of_the_day;
+    $image_path =~ s{(?=[.]png$)}{-$scale-$slug};
+
+    return $image_path;
 }
 
 
 sub border_flags {
-    my($self, $distro, $builder, $dist_colour) = @_;
+    my($self, $distro, $builder, $dist_ns) = @_;
 
-    my $colour  = $dist_colour->($distro);
+    my $colour  = $dist_ns->($distro);
     my $row     = $distro->row;
     my $col     = $distro->col;
 
@@ -156,22 +210,22 @@ sub border_flags {
 
     if($row > 0) {
         if(my $that = $builder->dist_at($row - 1, $col)) {
-            $flags &= (15 ^ NORTH) if $colour == $dist_colour->($that);
+            $flags &= (15 ^ NORTH) if $colour eq $dist_ns->($that);
         }
     }
 
     if($col > 0) {
         if(my $that = $builder->dist_at($row, $col - 1)) {
-            $flags &= (15 ^ WEST) if $colour == $dist_colour->($that);
+            $flags &= (15 ^ WEST) if $colour eq $dist_ns->($that);
         }
     }
 
     if(my $that = $builder->dist_at($row + 1, $col)) {
-        $flags &= (15 ^ SOUTH) if $colour == $dist_colour->($that);
+        $flags &= (15 ^ SOUTH) if $colour eq $dist_ns->($that);
     }
 
     if(my $that = $builder->dist_at($row, $col + 1)) {
-        $flags &= (15 ^ EAST) if $colour == $dist_colour->($that);
+        $flags &= (15 ^ EAST) if $colour eq $dist_ns->($that);
     }
 
     return $flags;
@@ -179,36 +233,37 @@ sub border_flags {
 
 
 sub draw_distro {
-    my($self, $im, $col, $row, $colour, $borders) = @_;
+    my($self, $im, $col, $row, $dist_colour, $borders) = @_;
 
-    my $scale = $self->scale;
+    my $colour = $self->colour;
+    my $scale  = $self->scale;
 
-    my $x1 = $col * $scale;
-    my $y1 = $row * $scale;
+    my $x1 = $self->x_offset + $col * $scale;
+    my $y1 = $self->y_offset + $row * $scale;
     my $x2 = $x1 + $scale - 1;
     my $y2 = $y1 + $scale - 1;
-    $im->filledRectangle($x1, $y1, $x2, $y2, $colour);
+    $im->filledRectangle($x1, $y1, $x2, $y2, $dist_colour);
 
     if($scale > 6) {
         if($borders & NORTH) {
-            $im->rectangle($x1, $y1 - 1, $x2, $y1, $border_colour);
+            $im->rectangle($x1, $y1 - 1, $x2, $y1, $colour->{border});
         }
         if($borders & SOUTH) {
-            $im->rectangle($x1, $y2, $x2, $y2 + 1, $border_colour);
+            $im->rectangle($x1, $y2, $x2, $y2 + 1, $colour->{border});
         }
         if($borders & EAST) {
-            $im->rectangle($x2, $y1, $x2 + 1, $y2, $border_colour);
+            $im->rectangle($x2, $y1, $x2 + 1, $y2, $colour->{border});
         }
         if($borders & WEST) {
-            $im->rectangle($x1 - 1, $y1, $x1, $y2, $border_colour);
+            $im->rectangle($x1 - 1, $y1, $x1, $y2, $colour->{border});
         }
     }
     else {
         if($borders & SOUTH) {
-            $im->line($x1, $y2, $x2, $y2, $border_colour);
+            $im->line($x1, $y2, $x2, $y2, $colour->{border});
         }
         if($borders & EAST) {
-            $im->line($x2, $y1, $x2, $y2, $border_colour);
+            $im->line($x2, $y1, $x2, $y2, $colour->{border});
         }
     }
 }
@@ -217,28 +272,18 @@ sub draw_distro {
 sub add_mass_label {
     my($self, $im, $font, $ns) = @_;
 
-    my $scale = $self->scale;
+    my $colour = $self->colour;
+    my $scale  = $self->scale;
 
-    my $x = $scale * $ns->label_x;
-    my $y = $scale * $ns->label_y;
-    my $w = $scale * $ns->label_w;
-    my $h = $scale * $ns->label_h;
-    if(0) {
-        $im->rectangle(
-            $x - $w / 2,
-            $y - $h / 2,
-            $x + $w / 2,
-            $y + $h / 2,
-            $map_colour[0]
-        );
-    }
+    my $x = $self->x_offset + $scale * $ns->label_x;
+    my $y = $self->y_offset + $scale * $ns->label_y;
 
     my $name = $ns->name or return;
     my $size = $self->font_size_from_mass($ns);
     return if $size < 7; # Don't bother if it's unreadably small
 
     my @bounds = GD::Image->stringFT(
-        $label_colour, $font, $size, 0, 0, 0, $name
+        $colour->{label}, $font, $size, 0, 0, 0, $name
     );
     my $width  = abs($bounds[2] - $bounds[0]);
     my $height = abs($bounds[7] - $bounds[1]);
@@ -260,16 +305,18 @@ sub add_mass_label {
     );
 
     # Don't anti-alias small font sizes (negative colour index)
-    my $text_colour = $size < 10 ? $thin_label_colour * -1 : $label_colour;
+    my $text_colour = $size < 10 ? $colour->{thin_label} * -1 : $colour->{label};
 
     foreach my $delta (
         ($size < 10 ? @thin_border : @thick_border),
         [ 0, 0 ],
     ) {
         my($delta_x, $delta_y) = @$delta;
-        my $colour = ($delta_x | $delta_y) ? $shadow_colour : $text_colour;
+        my $stroke_colour = ($delta_x | $delta_y)
+                          ? $colour->{shadow}
+                          : $text_colour;
         $im->stringFT(
-            $colour,
+            $stroke_colour,
             $font,
             $size,
             0,
@@ -291,6 +338,8 @@ sub font_size_from_mass {
     return( ($mass/109 + 5.9) * (13 + $scale) / 16 )
 }
 
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 
